c38dd263b1e2fa341de1848a3ed95f9fcd6e3e0f
given <given@ziqni.com>
2023-03-30 12:27:47 +0200

    Adding mock data

--- src/main/java/com/ziqni/transformer/test/store/ProductsStore.java
+++ src/main/java/com/ziqni/transformer/test/store/ProductsStore.java
@@ -2,6 +2,8 @@ package com.ziqni.transformer.test.store;
 
 import com.github.benmanes.caffeine.cache.*;
 import com.ziqni.admin.sdk.ZiqniAdminApiFactory;
+import com.ziqni.admin.sdk.model.ActionTypeAdjustmentFactor;
+import com.ziqni.admin.sdk.model.Member;
 import com.ziqni.admin.sdk.model.Product;
 import com.ziqni.admin.sdk.model.Result;
 import com.ziqni.transformer.test.concurrent.ZiqniConcurrentHashMap;
@@ -13,13 +15,16 @@ import scala.collection.Map;
 import scala.collection.Seq;
 
 import javax.annotation.Nullable;
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProductsStore implements AsyncCacheLoader<@NonNull String, @NonNull Product>, RemovalListener<@NonNull String, @NonNull Product> {
 
+    private final static AtomicInteger identifierCounter = new AtomicInteger();
     private static final ZiqniConcurrentHashMap<String, String> refIdCache = new ZiqniConcurrentHashMap<>();
     public final AsyncLoadingCache<@org.checkerframework.checker.nullness.qual.NonNull String, @org.checkerframework.checker.nullness.qual.NonNull Product> cache;
 
@@ -60,7 +65,19 @@ public class ProductsStore implements AsyncCacheLoader<@NonNull String, @NonNull
     }
 
     public Product makeMock(){
-
+        final var identifierCount = identifierCounter.incrementAndGet();
+        return new Product()
+                .id("memb-" + identifierCount)
+                .spaceName("test-space-name")
+                .created(OffsetDateTime.now())
+                .customFields(java.util.Map.of("test-custom-field","test-val"))
+                .addTagsItem("test-tag")
+                .metadata(java.util.Map.of("test-metadata","test-val"))
+                .name("Test-product")
+                .description("Test-description")
+                .adjustmentFactor(2.0)
+                .productRefId("test-product-ref-id")
+                .addActionTypeAdjustmentFactorsItem(new ActionTypeAdjustmentFactor().actionTypeId("test-action-type-id").adjustmentFactor(2.0));
     }
 
     @Override
--- src/main/java/com/ziqni/transformer/test/store/RewardStore.java
+++ src/main/java/com/ziqni/transformer/test/store/RewardStore.java
@@ -1,7 +1,10 @@
 package com.ziqni.transformer.test.store;
 
 import com.github.benmanes.caffeine.cache.*;
+import com.ziqni.admin.sdk.model.EntityType;
+import com.ziqni.admin.sdk.model.Product;
 import com.ziqni.admin.sdk.model.Reward;
+import com.ziqni.admin.sdk.model.RewardTypeReduced;
 import com.ziqni.transformer.test.concurrent.ZiqniExecutors;
 import com.ziqni.transformer.test.models.BasicReward;
 import lombok.NonNull;
@@ -9,13 +12,16 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RewardStore implements AsyncCacheLoader<@NonNull String, @NonNull Reward>, RemovalListener<@NonNull String, @NonNull Reward> {
 
+    private final static AtomicInteger identifierCounter = new AtomicInteger();
     private static final Logger logger = LoggerFactory.getLogger(RewardStore.class);
 
     public final AsyncLoadingCache<@NonNull String, @NonNull Reward> cache = Caffeine
@@ -34,6 +40,26 @@ public class RewardStore implements AsyncCacheLoader<@NonNull String, @NonNull R
     }
 
     public Reward makeMock(){
+        final var identifierCount = identifierCounter.incrementAndGet();
+        return new Reward()
+                .id("memb-" + identifierCount)
+                .spaceName("test-space-name")
+                .created(OffsetDateTime.now())
+                .customFields(java.util.Map.of("test-custom-field","test-val"))
+                .addTagsItem("test-tag")
+                .metadata(java.util.Map.of("test-metadata","test-val"))
+                .name("Test-product")
+                .description("Test-description")
+                .addConstraintsItem("test-constraint")
+                .delay(3)
+                .icon("test-icon")
+                .entityId("test-entity-id")
+                .entityType(EntityType.ACHIEVEMENT)
+                .issueLimit(3)
+                .period(3)
+                .pointInTime(OffsetDateTime.now())
+                .rewardRank("test-reward-rank")
+                .rewardType(new RewardTypeReduced().spaceName("test-space-name").id("reward-type-id"+identifierCount).key("test-key"));
 
     }
 
