c6f4bc369cb1eb096d078bd235b91f60f9ffbbb3
GivenTP-ziqni <given@ziqni.com>
2023-04-03 14:17:42 +0200

    pushEvent wip

--- src/main/java/com/ziqni/transformer/test/store/EventsStore.java
+++ src/main/java/com/ziqni/transformer/test/store/EventsStore.java
@@ -44,33 +44,40 @@ public class EventsStore implements CacheLoader<@NonNull String, EventsStore.Eve
     public CompletableFuture<ModelApiResponse> pushEvent(List<BasicEventModel> basicEventModels) {
         var response = new ModelApiResponse();
         basicEventModels.forEach(x -> {
-            if (x.action().equalsIgnoreCase(EntityType.MEMBER.getValue())){
-                CompletableFuture<Optional<String>> createdMember = membersStore.create(x.memberRefId(), "member-" + identifierCounter, x.tags(), null);
-                createdMember.thenAccept(y -> {
-                    y.ifPresent(z -> {
-                        response.addResultsItem(new Result().id(z));
-                    });
-                });
-
-            } else if (x.action().equalsIgnoreCase(EntityType.PRODUCT.getValue())) {
-                CompletableFuture<Optional<String>> createdProduct = productsStore.create(x.entityRefId(), "product-" + identifierCounter, null, null, null, null);
-                createdProduct.thenAccept(y -> {
-                    y.ifPresent(z -> {
-                        response.addResultsItem(new Result().id(z));
-                    });
-                });
-            }
+            pushEvent(x, response);
         });
 
         return CompletableFuture.completedFuture(response);
     }
 
     public CompletableFuture<ModelApiResponse> pushEventTransaction(BasicEventModel basicEventModel) {
-        return null;
+        var response = new ModelApiResponse();
+        pushEvent(basicEventModel, response);
+
+        return CompletableFuture.completedFuture(response);
     }
 
     public CompletableFuture<List<BasicEventModel>> findByBatchId(String batchId) {
         return null;
+    }
+
+    private void pushEvent(BasicEventModel basicEventModel, ModelApiResponse response) {
+        if (basicEventModel.action().equalsIgnoreCase(EntityType.MEMBER.getValue())){
+            CompletableFuture<Optional<String>> createdMember = membersStore.create(basicEventModel.memberRefId(), "member-" + identifierCounter, basicEventModel.tags(), null);
+            createdMember.thenAccept(y -> {
+                y.ifPresent(z -> {
+                    response.addResultsItem(new Result().id(z));
+                });
+            });
+
+        } else if (basicEventModel.action().equalsIgnoreCase(EntityType.PRODUCT.getValue())) {
+            CompletableFuture<Optional<String>> createdProduct = productsStore.create(basicEventModel.entityRefId(), "product-" + identifierCounter, null, null, null, null);
+            createdProduct.thenAccept(y -> {
+                y.ifPresent(z -> {
+                    response.addResultsItem(new Result().id(z));
+                });
+            });
+        }
     }
 
     public EventTransaction makeMock(){
