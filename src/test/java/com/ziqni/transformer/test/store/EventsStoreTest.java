4b03b3cfe0f0cc1886b97f8ff7d071585f665951
GivenTP-ziqni <given@ziqni.com>
2023-04-12 10:50:20 +0200

    update on EventsStore tests

--- src/test/java/com/ziqni/transformer/test/store/EventsStoreTest.java
+++ src/test/java/com/ziqni/transformer/test/store/EventsStoreTest.java
@@ -1,12 +1,16 @@
 package com.ziqni.transformer.test.store;
 
 import com.ziqni.transformers.domain.BasicEventModel;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.Test;
 import scala.Option;
 import scala.Some;
 import scala.collection.JavaConverters;
+import scala.collection.immutable.Seq;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,7 +28,9 @@ class EventsStoreTest {
 
     @Test
     void pushEvent() {
-        final var basicEventModel = new BasicEventModel(new Some<>("memb-2"), "test-member-ref-id-1", null, "test-event-1", new Some<>("1002"), "test-action", 2.0, null, null, null, null);
+        Seq<String> tags = JavaConverters.asScalaIteratorConverter(List.of("test-tags").iterator()).asScala().toSeq();
+//        var metadata = JavaConverters.asScalaIteratorConverter(Collections.emptyIterator()).asScala().toSeq();
+        final var basicEventModel = new BasicEventModel(new Some<>("memb-2"), "test-member-ref-id-1", "test-entity-ref-id", "test-event-1", new Some<>("1002"), "test-action", 2.0, DateTime.now(), tags, null, null);
         final var productFuture = eventsStore.pushEvent(basicEventModel);
         final var eventResponse = productFuture.join();
         assertNotNull(eventResponse);
@@ -33,7 +39,8 @@ class EventsStoreTest {
 
     @Test
     void pushEventTransaction() {
-        final var basicEventModel = new BasicEventModel(new Some<>("memb-2"), "test-member-ref-id-1", null, "test-event-1", new Some<>("1002"), "test-action", 2.0, null, null, null, null);
+        Seq<String> tags = JavaConverters.asScalaIteratorConverter(List.of("test-tags").iterator()).asScala().toSeq();
+        final var basicEventModel = new BasicEventModel(new Some<>("memb-2"), "test-member-ref-id-1", "test-entity-ref-id", "test-event-1", new Some<>("1002"), "test-action", 2.0, DateTime.now(), tags, null, null);
         final var productFuture = eventsStore.pushEventTransaction(basicEventModel);
         final var eventResponse = productFuture.join();
         assertNotNull(eventResponse);
